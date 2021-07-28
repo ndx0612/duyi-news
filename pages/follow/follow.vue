c4a876b7c1d13b5126ca17c7e296244a82edd0bd
hangang <hg9558@126.com>
2021-07-28 13:44:55 +0800

    第一天学习课程内容提交

---
+++ pages/follow/follow.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		follow page
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<view class="container">
+			<text>left</text>
+			<text>center</text>
+			<text>right</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		
+	}
+</script>
+
+<style scoped lang="scss">
+.container {
+	@include flex(flex-start,column);
+	color: $base-color;
+}
+</style>
