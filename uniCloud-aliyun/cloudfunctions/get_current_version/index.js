547206f36e51ad04580c82d4269edbe5e58d6971
alan <hg9558@126.com>
2021-08-19 19:04:32 +0800

    完成一版内容制作，调整没有数据的时候的显示

--- uniCloud-aliyun/cloudfunctions/get_current_version/index.js
+++ uniCloud-aliyun/cloudfunctions/get_current_version/index.js
@@ -9,7 +9,7 @@ exports.main = async (event, context) => {
   return {
     code: 0,
     data: {
-      version: res.data[0].current_version,
+      version: version.data[0].current_version,
       downLoadLinkUrl: 'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-c1da2723-930f-4455-86c1-df51096d1e3e/caee9db5-1291-499b-bf51-fc5bfb95a38e.apk'
     }
   }
